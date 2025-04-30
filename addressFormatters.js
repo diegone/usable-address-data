55e1bd3addaffdd0b6d0418bcee09b13480cac8b
Diego Lagunas <diegone@gmail.com>
2025-04-30 11:12:47 -0700

    Refresh with latest data.

--- addressFormatters.js
+++ addressFormatters.js
@@ -20,11 +20,11 @@ function f18(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`${a.Z}, ${a.C}`,`${a.S}`]}
 function f19(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`CH-${a.Z} ${a.C}`]}
 function f20(a) { return [`${a.N}`,`${a.O}`,`${a.X} ${a.A} ${a.C} ${a.X}`]}
 function f21(a) { return [`${a.Z}`,`${a.S}${a.C}${a.D}`,`${a.A}`,`${a.O}`,`${a.N}`]}
-function f22(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}, ${a.S}, ${a.Z}`]}
+function f22(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C}, ${a.S}, ${a.Z}`]}
 function f23(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.S}, ${a.C}`,`${a.Z}`]}
 function f24(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} ${a.S}`,`${a.Z}`]}
-function f25(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`,`${a.S}`,`${a.Z}`]}
-function f26(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.C} ${a.S}`]}
+function f25(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.C} ${a.S}`]}
+function f26(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`,`${a.S}`,`${a.Z}`]}
 function f27(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`FI-${a.Z} ${a.C}`]}
 function f28(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`FO${a.Z} ${a.C}`]}
 function f29(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`,`GUERNSEY`,`${a.Z}`]}
@@ -50,31 +50,33 @@ function f48(a) { return [`${a.Z}`,`${a.S}`,`${a.C}`,`${a.A}`,`${a.O}`,`${a.N}`]
 function f49(a) { return [`${a.S} ${a.C}${a.D}`,`${a.A}`,`${a.O}`,`${a.N}`,`${a.Z}`]}
 function f50(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.S} ${a.Z}`]}
 function f51(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`FL-${a.Z} ${a.C}`]}
-function f52(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`LT-${a.Z} ${a.C}`]}
+function f52(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`LT-${a.Z} ${a.C} ${a.S}`]}
 function f53(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`L-${a.Z} ${a.C}`]}
-function f54(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}, ${a.Z}`]}
+function f54(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.S}`,`${a.C}, ${a.Z}`]}
 function f55(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`MC-${a.Z} ${a.C} ${a.X}`]}
 function f56(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`MD-${a.Z} ${a.C}`]}
-function f57(a) { return [`${a.A}`,`${a.O}`,`${a.N}`]}
-function f58(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.Z} ${a.C}, ${a.S}`]}
-function f59(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.Z} ${a.C}`,`${a.S}`]}
-function f60(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.C}${a.S}`]}
-function f61(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C} ${a.Z}`,`${a.S}`]}
-function f62(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z}`,`${a.C}, ${a.S}`]}
-function f63(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C} ${a.Z}`]}
-function f64(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`,`${a.S}`]}
-function f65(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} ${a.Z} ${a.S}`]}
-function f66(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}, ${a.C}`,`${a.Z} ${a.S}`]}
-function f67(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}-${a.Z}`]}
-function f68(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} PR ${a.Z}`]}
-function f69(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`SE-${a.Z} ${a.C}`]}
-function f70(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`SINGAPORE ${a.Z}`]}
-function f71(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`SI-${a.Z} ${a.C}`]}
-function f72(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z}-${a.C}`,`${a.S}`]}
-function f73(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D} ${a.C}`,`${a.S} ${a.Z}`]}
-function f74(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.C}/${a.S}`]}
-function f75(a) { return [`${a.Z}`,`${a.S}${a.C}`,`${a.A}`,`${a.O}`,`${a.N}`]}
-function f76(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} ${a.Z}, ${a.S}`]}
-function f77(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C}`,`${a.Z}`]}
-function f78(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`]}
-export const addressFormatters = {AC:f0,AF:f0,AI:f0,FK:f0,GB:f0,IM:f0,IO:f0,KE:f0,LK:f0,NA:f0,PN:f0,SD:f0,SH:f0,SZ:f0,TA:f0,TC:f0,VG:f0,AD:f1,BA:f1,BG:f1,CY:f1,CZ:f1,DE:f1,DK:f1,DO:f1,DZ:f1,EE:f1,EH:f1,ET:f1,GE:f1,GL:f1,GR:f1,GW:f1,IS:f1,KG:f1,KW:f1,LA:f1,LR:f1,MA:f1,ME:f1,MG:f1,MK:f1,NE:f1,NO:f1,PL:f1,PT:f1,PY:f1,RO:f1,RS:f1,SJ:f1,SK:f1,SM:f1,SN:f1,TJ:f1,TM:f1,TN:f1,TZ:f1,VA:f1,XK:f1,ZM:f1,AE:f2,NR:f2,AL:f3,EC:f3,MU:f3,OM:f3,AM:f4,AR:f5,CL:f5,CV:f5,UZ:f5,AS:f6,CA:f6,FM:f6,MH:f6,MP:f6,PW:f6,UM:f6,VI:f6,AT:f7,BE:f7,FR:f7,NL:f7,AU:f8,CC:f8,CX:f8,HM:f8,NF:f8,AX:f9,AZ:f10,BB:f11,SO:f11,US:f11,BD:f12,BF:f13,MW:f13,BH:f14,BM:f14,BN:f14,BT:f14,GU:f14,IL:f14,JO:f14,KH:f14,LB:f14,LS:f14,MT:f14,MV:f14,NP:f14,SA:f14,VC:f14,BL:f15,GF:f15,GP:f15,MF:f15,MQ:f15,NC:f15,PM:f15,RE:f15,WF:f15,YT:f15,BR:f16,BS:f17,KN:f17,BY:f18,CH:f19,CI:f20,CN:f21,CO:f22,CR:f23,CU:f24,EG:f25,RU:f25,UA:f25,ES:f26,IT:f26,PF:f26,UY:f26,FI:f27,FO:f28,GG:f29,GI:f30,GN:f31,GS:f32,GT:f33,HK:f34,HN:f35,HR:f36,HT:f37,HU:f38,ID:f39,MN:f39,VN:f39,IE:f40,IN:f41,PE:f41,IQ:f42,IR:f43,JE:f44,JM:f45,JP:f46,KI:f47,KP:f48,KZ:f48,KR:f49,KY:f50,LI:f51,LT:f52,LU:f53,LV:f54,MM:f54,MC:f55,MD:f56,MO:f57,MX:f58,MY:f59,MZ:f60,NG:f61,NI:f62,NZ:f63,PA:f64,SC:f64,SR:f64,TV:f64,PG:f65,PH:f66,PK:f67,PR:f68,SE:f69,SG:f70,SI:f71,SV:f72,TH:f73,TR:f74,TW:f75,VE:f76,ZA:f77,ZZ:f78};
+function f57(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}, ${a.Z}`]}
+function f58(a) { return [`${a.A}`,`${a.O}`,`${a.N}`]}
+function f59(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.Z} ${a.C}, ${a.S}`]}
+function f60(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.Z} ${a.C}`,`${a.S}`]}
+function f61(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.C}${a.S}`]}
+function f62(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C} ${a.Z}`,`${a.S}`]}
+function f63(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z}`,`${a.C}, ${a.S}`]}
+function f64(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C} ${a.Z}`]}
+function f65(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`,`${a.S}`]}
+function f66(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} ${a.Z} ${a.S}`]}
+function f67(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}, ${a.C}`,`${a.Z} ${a.S}`]}
+function f68(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C}-${a.Z}`]}
+function f69(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} PR ${a.Z}`]}
+function f70(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.S} ${a.C}`]}
+function f71(a) { return [`${a.O}`,`${a.N}`,`${a.A}`,`SE-${a.Z} ${a.C}`]}
+function f72(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`SINGAPORE ${a.Z}`]}
+function f73(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`SI-${a.Z} ${a.C}`]}
+function f74(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z}-${a.C}`,`${a.S}`]}
+function f75(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D} ${a.C}`,`${a.S} ${a.Z}`]}
+function f76(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.Z} ${a.C}/${a.S}`]}
+function f77(a) { return [`${a.Z}`,`${a.S}${a.C}`,`${a.A}`,`${a.O}`,`${a.N}`]}
+function f78(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C} ${a.Z}, ${a.S}`]}
+function f79(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.D}`,`${a.C}`,`${a.Z}`]}
+function f80(a) { return [`${a.N}`,`${a.O}`,`${a.A}`,`${a.C}`]}
+export const addressFormatters = {AC:f0,AF:f0,AI:f0,FK:f0,GB:f0,IM:f0,IO:f0,KE:f0,LK:f0,NA:f0,PN:f0,SD:f0,SH:f0,SZ:f0,TA:f0,TC:f0,VG:f0,AD:f1,BA:f1,BG:f1,CY:f1,CZ:f1,DE:f1,DK:f1,DO:f1,DZ:f1,EH:f1,ET:f1,GE:f1,GL:f1,GR:f1,GW:f1,IS:f1,KG:f1,KW:f1,LA:f1,LR:f1,MA:f1,ME:f1,MG:f1,MK:f1,NE:f1,NO:f1,PL:f1,PT:f1,PY:f1,RS:f1,SJ:f1,SK:f1,SM:f1,SN:f1,TJ:f1,TM:f1,TN:f1,TZ:f1,VA:f1,XK:f1,ZM:f1,AE:f2,NR:f2,AL:f3,EC:f3,MU:f3,OM:f3,AM:f4,AR:f5,CL:f5,CV:f5,UZ:f5,AS:f6,CA:f6,FM:f6,MH:f6,MP:f6,PW:f6,UM:f6,VI:f6,AT:f7,BE:f7,FR:f7,NL:f7,AU:f8,CC:f8,CX:f8,HM:f8,NF:f8,AX:f9,AZ:f10,BB:f11,SO:f11,US:f11,BD:f12,BF:f13,MW:f13,BH:f14,BM:f14,BN:f14,BT:f14,GU:f14,IL:f14,JO:f14,KH:f14,LB:f14,LS:f14,MT:f14,MV:f14,NP:f14,SA:f14,VC:f14,BL:f15,GF:f15,GP:f15,MF:f15,MQ:f15,NC:f15,PM:f15,RE:f15,WF:f15,YT:f15,BR:f16,BS:f17,KN:f17,BY:f18,CH:f19,CI:f20,CN:f21,CO:f22,CR:f23,CU:f24,EE:f25,ES:f25,IT:f25,PF:f25,UY:f25,EG:f26,RU:f26,UA:f26,FI:f27,FO:f28,GG:f29,GI:f30,GN:f31,GS:f32,GT:f33,HK:f34,HN:f35,HR:f36,HT:f37,HU:f38,ID:f39,MN:f39,VN:f39,IE:f40,IN:f41,PE:f41,IQ:f42,IR:f43,JE:f44,JM:f45,JP:f46,KI:f47,KP:f48,KZ:f48,KR:f49,KY:f50,LI:f51,LT:f52,LU:f53,LV:f54,MC:f55,MD:f56,MM:f57,MO:f58,MX:f59,MY:f60,MZ:f61,NG:f62,NI:f63,NZ:f64,PA:f65,SC:f65,SR:f65,TV:f65,PG:f66,PH:f67,PK:f68,PR:f69,RO:f70,SE:f71,SG:f72,SI:f73,SV:f74,TH:f75,TR:f76,TW:f77,VE:f78,ZA:f79,ZZ:f80};
